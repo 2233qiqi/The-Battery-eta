8103f9574ada34697be5579e9605955b4ad79741
2233qiqi <1091682015@qq.com>
2025-07-20 19:47:46 +0800

    19.47

--- src/SingleParticleSD.cc
+++ src/SingleParticleSD.cc
@@ -127,7 +127,7 @@ void SingleParticleSD::SaveToROOT()
     TGraph *graph = new TGraph(fNbins, x_depth.data(), y_total_edep.data());
 
     graph->SetName("TotalEnergyDepositVsDepth_Unfiltered");
-    graph->SetTitle("Engery and depth;Depth [um];Total Energy Deposited [keV]");
+    graph->SetTitle("5umNI-Engery and depth;Depth [um];Total Energy Deposited [keV]");
 
     graph->SetMarkerStyle(20);
     graph->SetMarkerColor(kBlue);
