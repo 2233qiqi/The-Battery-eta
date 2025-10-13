8c03f8ea52d1ed063f98a8ad295db284628159e0
2233qiqi <2233qiqi>
2025-10-13 17:09:20 +0800

    10/13

--- src/PrimaryGenertor.cc
+++ src/PrimaryGenertor.cc
@@ -3,7 +3,7 @@
 #include "G4ParticleGun.hh"
 #include "G4IonTable.hh"
 #include "G4ParticleDefinition.hh"
-#include "G4Geantino.hh"
+#include "G4Geantino.hh"    
 #include "G4SystemOfUnits.hh"
 #include "G4Event.hh"
 #include "G4Exception.hh"
