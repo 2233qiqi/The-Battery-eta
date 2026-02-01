e4ea6f32958f19f3a897c44760354a607086bfe2
2233qiqi <1091682015@qq.com>
2026-02-01 21:29:47 +0800

    2/1

--- smcd.cc
+++ smcd.cc
@@ -1,87 +1,34 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-
 #include "G4RunManager.hh"
 #include "G4UImanager.hh"
-#include "G4UIExecutive.hh"
-#include "G4VisManager.hh"
 #include "G4VisExecutive.hh"
-#include "G4NuclideTable.hh"
+#include "G4UIExecutive.hh"
+#include "G4MTRunManager.hh"
 
 #include "DetectorConstruction.hh"
-#include "ActionInitialization.hh"
 #include "PhysicsList.hh"
+#include "ActionInitialization.hh"
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
-    
-    std::ofstream logfile("run.log");
-    G4cout.rdbuf(logfile.rdbuf());
-    
-    G4UIExecutive *ui = nullptr;
-    if (argc == 1)
-    {
-        ui = new G4UIExecutive(argc, argv);
-    }
+    auto* runManager = new G4MTRunManager;
 
-    auto *runManager = new G4RunManager;
-
-    runManager->SetUserInitialization(new PhysicsList());
     runManager->SetUserInitialization(new DetectorConstruction());
+    runManager->SetUserInitialization(new PhysicsList());
     runManager->SetUserInitialization(new ActionInitialization());
 
-    runManager->Initialize();
-
-    G4VisManager *visManager = new G4VisExecutive("Quiet");
+    auto* visManager = new G4VisExecutive;
     visManager->Initialize();
 
-    G4UImanager *uiManager = G4UImanager::GetUIpointer();
-   
-    if(argc == 4)
-    {
-        int particle_count=std::stoi(argv[1]);
-        std::string particle_type = argv[2];
-        std::string particle_direction = argv[3];
-        
-        std::cout << "Running in batch mode with " << particle_count 
-        <<"  and particle_type  "<<particle_type
-        <<"  and particle_direction  "<<particle_direction
-        <<std::endl;
-     //设置粒子类别
-       G4String particleCmd = "/gps/particle " + particle_type;
-       uiManager->ApplyCommand(particleCmd);
-     //判断粒子方向
-       if (particle_direction == "iso")
-       {
- 
-        uiManager->ApplyCommand("/gps/ang/type iso");
-   
-       }
-       else
-       {
-        G4String directionCmd = "/gps/direction " + particle_direction;
-        uiManager->ApplyCommand(directionCmd);
-    
-        uiManager->ApplyCommand("/gps/ang/type direction");
-       }
-     //运行对应的宏文件
-        uiManager->ApplyCommand("/control/execute run.mac"); 
+    auto* ui = new G4UIExecutive(argc, argv);
 
-         G4String beamCmd = "/run/beamOn " + std::to_string(particle_count);
-            uiManager->ApplyCommand(beamCmd);
-    }
+    auto* UImanager = G4UImanager::GetUIpointer();
 
-    else
-    {
-        std::cerr << "\nERROR: Incorrect number of arguments. Expected 3, got " << argc - 1 << ".\n";
-        std::cerr << "Usage: ./exampleB1  <N_events> <ParticleType> \"<Dir X Y Z>\"\n";
-        std::cerr << "Example: ./exampleB1 10000 gamma \"0 0 1\"\n" << std::endl;
-        
-    }
+    UImanager->ApplyCommand("/control/execute run.mac");
 
+    ui->SessionStart();
+
+    delete ui;
     delete visManager;
     delete runManager;
-
     return 0;
 }
--- src/DetectorConstruction.cc
+++ src/DetectorConstruction.cc
@@ -20,6 +20,9 @@ DetectorConstruction :: ~DetectorConstruction()
 
 G4VPhysicalVolume* DetectorConstruction ::Construct()
 {
+
+    G4bool checkOverlaps = true;
+
     G4NistManager* nist = G4NistManager::Instance();
     auto *WorldMat = nist ->FindOrBuildMaterial("G4_AIR");
     auto *NiMat = nist ->FindOrBuildMaterial("G4_Ni");
@@ -28,11 +31,10 @@ G4VPhysicalVolume* DetectorConstruction ::Construct()
 
     //SIC matrial
     G4double density = 3.21 * g/cm3;
-    G4int ncomponents = 2;
     G4int natoms;
-    G4Material *Sic = new G4Material("SiliconCarbide", density, ncomponents);
-    Sic ->AddElement(SiMat, natoms=1);
-    Sic ->AddElement(CMat,natoms=1);
+    G4Material *Sic = new G4Material("SiliconCarbide", density, 2);
+    Sic->AddElement(SiMat, 1); 
+    Sic->AddElement(CMat, 1);
 
     //World
     G4double World_X = 10*cm;
@@ -41,7 +43,7 @@ G4VPhysicalVolume* DetectorConstruction ::Construct()
 
     G4Box* SolidWolrd  = new G4Box("World",World_X/2,World_Y/2,World_Z/2);
     G4LogicalVolume* LogicalWorld = new G4LogicalVolume(SolidWolrd,WorldMat,"LogicalWorld");
-    G4VPhysicalVolume* PhysicalWorld = new G4PVPlacement(0,G4ThreeVector(0,0,0),"PhysicalWorld",LogicalWorld,NULL,false,0,true);
+    G4VPhysicalVolume* PhysicalWorld = new G4PVPlacement(0,G4ThreeVector(0,0,0),LogicalWorld,"PhysicalWorld",NULL,false,0,true);
 
     //Ni
     G4double Ni_X = 1*cm;
@@ -50,7 +52,7 @@ G4VPhysicalVolume* DetectorConstruction ::Construct()
 
     G4Box* SolidNi = new G4Box ("Ni",Ni_X/2,Ni_Y/2,Ni_Z/2);
     G4LogicalVolume* LogicalNi = new G4LogicalVolume (SolidNi,NiMat,"LogicalNi");
-    G4VPhysicalVolume* PhysicalNi  = new G4PVPlacement(0,G4ThreeVector(0,0,0),"PhysicalNi",LogicalNi,PhysicalWorld,false,0,true);
+    G4VPhysicalVolume* PhysicalNi  = new G4PVPlacement(0,G4ThreeVector(0,0,0),LogicalNi,"PhysicalNi",LogicalWorld,false,0,true);
 
     //Sic
     G4double Sic_X = 1*cm;
@@ -58,8 +60,8 @@ G4VPhysicalVolume* DetectorConstruction ::Construct()
     G4double Sic_Z = 310.3*um;
 
     G4Box *SolidSic = new G4Box ("Sic",Sic_X/2,Sic_Y/2,Sic_Z/2);
-    G4LogicalVolume* LogiclaVolumeSic = new G4LogicalVolume(SolidSic,Sic,"LogicalSic");
-    G4VPhysicalVolume *PhysicalSic = new G4PVPlacement (0,G4ThreeVector(0,0,(Ni_Z/2+Sic_Z/2)),"PhySicalSic",LogiclaVolumeSic,PhysicalWorld,false,0,true);
+    G4LogicalVolume* LogicalVolumeSic = new G4LogicalVolume(SolidSic,Sic,"LogicalSic");
+    G4VPhysicalVolume *PhysicalSic = new G4PVPlacement (0,G4ThreeVector(0,0,(Ni_Z/2+Sic_Z/2)),LogicalVolumeSic,"PhysicalSic",LogicalWorld,false,0,true);
 
     //可视化
     G4VisAttributes* Ni_VisAtt = new G4VisAttributes(G4Color(0.5,0.5,0.5,0.7));
@@ -67,7 +69,7 @@ G4VPhysicalVolume* DetectorConstruction ::Construct()
     LogicalNi ->SetVisAttributes(Ni_VisAtt);
     G4VisAttributes* Sic_VisAtt = new G4VisAttributes(G4Color(0.0,1.0,0.0,0.7));
     Sic_VisAtt ->SetForceSolid(true);
-    LogiclaVolumeSic ->SetVisAttributes(Sic_VisAtt);
+    LogicalVolumeSic ->SetVisAttributes(Sic_VisAtt);
     
     return PhysicalWorld;
 
