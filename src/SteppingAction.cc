e96953fa3f8ce8246fda11063dcc46b4ad679261
2233qiqi <1091682015@qq.com>
2026-02-01 20:20:19 +0800

    2/1

--- src/SteppingAction.cc
+++ src/SteppingAction.cc
@@ -9,3 +9,25 @@
 #include "G4RunManager.hh"
 #include "G4LogicalVolume.hh"
 
+SteppingAction::SteppingAction(EventAction* eventAction):G4UserSteppingAction(), fEventAction(eventAction), fScoringVolume(NULL)
+{
+
+}
+
+SteppingAction :: ~SteppingAction()
+{
+
+}
+
+void SteppingAction::UserSteppingAction(const G4Step* step)
+{  
+  G4LogicalVolume* volume = step->GetPreStepPoint()->GetTouchableHandle()->GetVolume()->GetLogicalVolume();
+
+    if (volume->GetName() == "GeActive_LV") {
+        G4double edep = step->GetTotalEnergyDeposit();
+
+        if (edep > 0.) {
+            fEventAction->AddEdep(edep);
+        }
+    }
+}
